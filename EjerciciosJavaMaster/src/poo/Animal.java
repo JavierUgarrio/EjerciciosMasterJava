fb4c0526d1377bc8448529be47502e85024bdbf4
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-10-17 21:42:28 +0200

    poo animales

--- EjerciciosJavaMaster/src/poo/Animal.java
+++ EjerciciosJavaMaster/src/poo/Animal.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package poo;
 
-/**
- *
- * @author User
- */
 public class Animal {
+    public int patas;
+    public String nombre;
+    public String tipo;
+    
+    //Constructor
+
+    public Animal(int patas, String nombre, String tipo) {
+        this.patas = patas;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+    
+    //Get & Set
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    //toString
+
+    @Override
+    public String toString() {
+        return "Animal{" + "patas=" + patas + ", nombre=" + nombre + ", tipo=" + tipo + '}';
+    }
+    
     
 }
